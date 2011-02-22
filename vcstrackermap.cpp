d40855e69fb9fed3eee64ea6cfee5efbc0242454
nidujay@gmail.com <nidujay@gmail.com@7aca8338-beae-cf89-b0b1-2e0b104211d0>
2011-02-22 09:34:15 +0000

    Added Missing trackermap sources

---
+++ vcstrackermap.cpp
@@ -0,0 +1,72 @@
+#include "vcstrackermap.h"
+#include "vcsfactory.h"
+
+VcsTrackerMap::~VcsTrackerMap()
+{
+    std::map<const wxString, vcsProjectTracker*>::iterator i;
+
+    for(i=m_Map.begin(); i != m_Map.end(); i++)
+    {
+        Remove(i);
+    }
+}
+
+vcsProjectTracker* VcsTrackerMap::Find(const wxString& prjFilename) const
+{
+    std::map<const wxString, vcsProjectTracker*>::const_iterator i;
+
+    i = m_Map.find(prjFilename);
+    if(i == m_Map.end())
+    {
+        return 0;
+    }
+
+    return i->second;
+}
+
+bool VcsTrackerMap::CreateTracker(const wxString& prjFilename)
+{
+    if(Find(prjFilename))
+    {
+        return true;
+    }
+
+    IVersionControlSystem* vcs = VcsFactory::GetVcs(prjFilename);
+
+    if(!vcs)
+    {
+        // Project not controlled
+        return false;
+    }
+
+    vcsProjectTracker* prjTracker = new vcsProjectTracker(vcs, prjFilename);
+    m_Map[prjFilename] = prjTracker;
+
+    return true;
+}
+
+void VcsTrackerMap::Remove(std::map<const wxString, vcsProjectTracker*>::iterator i)
+{
+    vcsProjectTracker* tracker = i->second;
+    delete tracker;
+    m_Map.erase(i);
+}
+
+bool VcsTrackerMap::RemoveTracker(const wxString& prjFilename)
+{
+    std::map<const wxString, vcsProjectTracker*>::iterator i;
+
+    i = m_Map.find(prjFilename);
+    if(i == m_Map.end())
+    {
+        return false;
+    }
+
+    Remove(i);
+    return true;
+}
+
+vcsProjectTracker* VcsTrackerMap::GetTracker(const wxString& prjFilename)
+{
+    return Find(prjFilename);
+}
---
+++ vcstrackermap.h
@@ -0,0 +1,26 @@
+#ifndef VCSTRACKERMAP_H
+#define VCSTRACKERMAP_H
+
+#include "copyprotector.h"
+#include "vcsprojecttracker.h"
+
+class VcsTrackerMap : private CopyProtector
+{
+    public:
+        /** Default constructor */
+        VcsTrackerMap() {}
+        /** Default destructor */
+        virtual ~VcsTrackerMap();
+
+        bool CreateTracker(const wxString& prjFilename);
+        bool RemoveTracker(const wxString& prjFilename);
+        vcsProjectTracker* GetTracker(const wxString& prjFilename);
+    protected:
+
+    private:
+        std::map<const wxString, vcsProjectTracker*> m_Map;
+        vcsProjectTracker* Find(const wxString& prjFilename) const;
+        void Remove(std::map<const wxString, vcsProjectTracker*>::iterator);
+};
+
+#endif // VCSTRACKERMAP_H
