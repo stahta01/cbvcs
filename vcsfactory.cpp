42f716133e0517b7798beb5a75623d4a97ad7830
Christo Joseph <1538412+josephch@users.noreply.github.com>
2024-08-01 21:28:18 +0530

    Support both git binary and libgit2 based implementations

--- vcsfactory.cpp
+++ vcsfactory.cpp
@@ -16,16 +16,27 @@
     along with cbvcs.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include "vcsfactory.h"
-#include "git.h"
+#ifdef USE_LIBGIT2
 #include "git_libgit2.h"
+#else
+#include "git.h"
+#endif
 #include "icommandexecuter.h"
 
 /*static*/ IVersionControlSystem* VcsFactory::GetVcs(const wxString& ProjectPath,
                                                      ICommandExecuter& shellUtils)
 {
+#ifdef USE_LIBGIT2
     if(LibGit2::IsGitRepo(ProjectPath, shellUtils))
     {
         return new LibGit2(ProjectPath, shellUtils);
     }
+#else
+    if(git::IsGitRepo(ProjectPath, shellUtils))
+    {
+        return new git(ProjectPath, shellUtils);
+    }
+
+#endif
     return 0;
 }
